ebd50f02596cf1d0852d2885d634ed018544cc61
HelbertGuirra <helbert.guirra@gmail.com>
2025-05-23 18:36:06 -0300

    Corrige capitalização da pasta Formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Perfil from "./components/Perfil";
-import Formulario from "./components/Formulario";
+import Formulario from "./components/formulario";
 import ReposList from "./components/RepoList";
 function App() {
     const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true);
